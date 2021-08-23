5cde32bc54f0f9cf245ced5529528949f1c12f87
楊竣宇\yang <ch20026103@yahoo.com.tw>
2021-08-23 22:37:53 +0800

    add clock feature

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const remove = require("./db/methods/remove");
 const express = require("express");
 
 // cron
+var clock = new CronJob(
+  "0 29 * * * *",
+  async function () {
+    const { db } = await connectToDatabase();
+    const collection = db.collection("echo");
+    const result = await collection.insert({ time: new Date() });
+    console.log('時間到囉! app起床!');
+  },
+  null,
+  true,
+  "Asia/Taipei"
+);
+clock.start();
+
 let removeArr = ["0 0 8 * * *", "0 0 11 * * *"];
 let insertArr = ["0 1 8 * * *", "0 1 11 * * *"];
 
